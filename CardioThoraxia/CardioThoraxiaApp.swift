c5e929e59397b51d56a1e1ee85eb748317e27900
Edward Bender <ebender001@gmail.com>
2026-01-28 16:18:30 -0600

    Initial Commit

---
+++ CardioThoraxia/CardioThoraxiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardioThoraxiaApp.swift
+//  CardioThoraxia
+//
+//  Created by Edward Bender on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct CardioThoraxiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
